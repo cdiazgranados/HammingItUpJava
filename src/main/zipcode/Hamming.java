d2fa4159fc35bc356a4cccf2ee0dbd04b50214d3
Carolina Diazgranados <linacello@gmail.com>
2022-11-29 21:40:43 -0500

    done

--- src/main/zipcode/Hamming.java
+++ src/main/zipcode/Hamming.java
@@ -1,10 +1,29 @@
-package zipcode;
+package src.main.zipcode;
 
 public class Hamming {
-    public Hamming(String s, String s1) {
+    String s;
+    String s1;
+
+    public Hamming(String s, String s1) { //put the throw in the constructor
+        if (!(s.length() == s1.length())) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        } else {
+            this.s = s;
+            this.s1 = s1;
+        }
     }
 
     public int getHammingDistance() {
-        return -1;
+
+        int count = 0;
+        String[] sArray = s.split("");
+        String[] s1Array = s1.split("");
+        for (int i = 0; i < s.length(); i++) {
+            if (!sArray[i].equals(s1Array[i])) {
+                count++;
+            }
+        }
+        return count;
     }
+
 }
